46b563a5b65c20b91d14a91f8e57b2eda11f96cf
Hrishita <panhrishi@gmail.com>
2026-05-24 17:05:42 -0400

    Add working crypto price API route

--- backend/routes/prices.js
+++ backend/routes/prices.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+router.get("/test", (req, res) => {
+  res.json({ message: "Prices route is connected" });
+});
 const { getStockPrice } = require("../services/finnhubService");
 const { getCryptoPrice } = require("../services/coingeckoService");
 
